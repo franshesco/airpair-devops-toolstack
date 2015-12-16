718df3e847627950c2d5e843816ad4de3331863c
root <root@airpair-devops-toolstack>
2015-12-16 15:16:29 +0000

    chef-repo added

---
+++ chef-repo/site-cookbooks/factorialapp/recipes/default.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: factorialapp
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+app_path = "/opt/airpair-devops-toolstack"
+if not ::File.exists?(app_path)
+  app_path = "/vagrant"
+end
+
+package "python-pip"
+
+execute "install-python-module" do
+  command "pip install -r #{app_path}/requirements.txt"
+end
+
+if app_path != "/vagrant"
+  cookbook_file "/etc/init/factorial-api.conf" do
+    source "factorial-api.conf"
+  end
+  service "factorial-api" do
+    provider Chef::Provider::Service::Upstart
+    action [ :enable, :start ]
+  end
+end
